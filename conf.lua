67f06aa8fafb647693ee05a37d9bf8624e329571
Yann in 't Veld <yannintveld@gmail.com>
2025-04-08 01:05:56 +0200

    Changed title and added cover image

--- conf.lua
+++ conf.lua
@@ -10,7 +10,7 @@ function love.conf(t)
     t.audio.mic = false                 -- Request and use microphone capabilities in Android (boolean)
     t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
 
-    t.window.title = "LD57"         -- The window title (string)
+    t.window.title = "The Scourge from the Deep"         -- The window title (string)
     t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
     t.window.width = 1920                -- The window width (number)
     t.window.height = 1080               -- The window height (number)
